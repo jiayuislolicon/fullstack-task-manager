99529a9e444decd335092e6b3a93e45fd992f7f1
lolicon <lovesyndra@gmail.com>
2023-07-07 14:58:23 +0800

    feat: apply eslint

--- client/index.js
+++ client/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 // import "./index.css";
-import App from "./App";
+import App from './App';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
