21ea651bc1c31146111bc82de893475fd6a4aef8
lolicon <lovesyndra@gmail.com>
2023-07-07 19:47:28 +0800

    feat: connect db

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
-const os = require('os');
-
 const app = express();
+const connectDB = require('./db/connect');
+require('dotenv').config();
 
-app.use(express.static('dist'));
-app.get('/api/getUsername', (_, res) =>
-  res.send({ username: os.userInfo().username })
-);
+const port = process.env.PORT || 8080;
 
-app.listen(process.env.PORT || 8080, () =>
-  console.log(`Listening on port ${process.env.PORT || 8080}!`)
-);
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
